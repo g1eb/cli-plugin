7fa5a77b23c94e89915efda9fcaf48c13fcec718
Gleb <git@gleb.nl>
2016-12-14 17:46:47 +0100

    Add cli input and output elements to the container.

--- src/cli-plugin.js
+++ src/cli-plugin.js
@@ -8,18 +8,27 @@ var cliPlugin = {
   settings: {
   },
 
-
   /**
    * Initialize
    */
   init: function (config) {
-    this.createElement();
+    this.createElements();
   },
 
-  createElement: function () {
-    var element = document.createElement('div');
-    element.className = 'cli-plugin';
-    document.body.appendChild(element);
+  createElements: function () {
+    this.containerElement = document.createElement('div');
+    this.containerElement.setAttribute('class', 'cli-plugin');
+    document.body.appendChild(this.containerElement);
+
+    this.inputElement = document.createElement('div');
+    this.inputElement.setAttribute('class', 'input active');
+    this.inputElement.setAttribute('contenteditable', 'true');
+    this.inputElement.setAttribute('tabindex', '0');
+    this.containerElement.appendChild(this.inputElement);
+
+    this.outputElement = document.createElement('div');
+    this.outputElement.setAttribute('class', 'output');
+    this.containerElement.appendChild(this.outputElement);
   },
 
 
